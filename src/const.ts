92a7676829a78995e61e57ca36900ee4245cdd74
“avganka” <karambasterg@gmail.com>
2022-04-19 03:34:38 +0300

    Добавлены переменные маршрутов и городов

--- src/const.ts
+++ src/const.ts
@@ -10,3 +10,16 @@ export enum AuthStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export enum APIRoute {
+  Hotels = '/hotels',
+  Login = '/login',
+  Favorite = '/favorite',
+  Logout = '/logout',
+  Comments = '/comments',
+}
+
+
+export const CITIES_LIST = ['Paris', 'Amsterdam', 'Cologne', 'Dusseldorf', 'Hamburg', 'Brussels'];
+
+export const MOUNTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];

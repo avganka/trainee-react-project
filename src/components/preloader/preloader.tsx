9ebc7e40d3c38273920cb5f1c4faac2783d742bd
“avganka” <karambasterg@gmail.com>
2022-04-19 03:38:06 +0300

    Добавлен прелоадер

---
+++ src/components/preloader/preloader.tsx
@@ -0,0 +1,11 @@
+function Preloader ():JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <div className="container">
+        <p>Loading....</p>
+      </div>
+    </div>
+  );
+}
+
+export {Preloader};
